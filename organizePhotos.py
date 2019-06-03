d4678a34ef078d0fefb87be4e479f256133ca346
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-03 23:42:10 +0530

    Update organizePhotos.py

--- organizePhotos.py
+++ organizePhotos.py
@@ -12,10 +12,9 @@ path = "Photos"
 os.rename(filename, os.path.join(path, filename))
 
 def extract_place(filename):
-    parts = filename.split("_") # Get a list containing all the parts
-    place_name = parts[1] # Use the index operator to select the second list item
-    return place_name
+    return filename.split("_")[1]
     
+# some calls you can test it with:
 print(extract_place("2016-11-04_Berlin_09/42/22.jpg"))
 print(extract_place("2018-01-03_Oahu_21/51/57.jpg"))
 print(extract_place("2018-01_Scottland_11/51/27.jpg"))
