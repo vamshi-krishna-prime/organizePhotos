c84713f934d2ce2d51621628db1b79e3bcf691f1
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-03 23:43:49 +0530

    Update organizePhotos.py

--- organizePhotos.py
+++ organizePhotos.py
@@ -7,20 +7,23 @@ def make_directories(list):
     for location in list:
         os.mkdir(location)
 
-# navigating to the photos directory and listing the file names
-os.chdir("c:\\Users\VAMSI\Desktop\Python\organize photos\Photos")
-originals = os.listdir()
-print(originals)
-places = []
-# loop to extract place names
-for filename in originals:
-    place_name = extract_place(filename)
-    if place_name not in places:
-        places.append(place_name)
+def organize_photos(directory):
+    os.chdir(directory)
+    originals = os.listdir()
+    print(originals)
+    places = []
+    # loop to extract place names
+    for filename in originals:
+        place_name = extract_place(filename)
+        if place_name not in places:
+            places.append(place_name)
+    # print the list of places and make directories
+    print(places)
+    make_directories(places)
+    # move/organize the photos to the respective folders
+    for filename in originals:
+        place_name = extract_place(filename)
+        os.rename(filename, os.path.join(place_name, filename))
 
-print(places)
-make_directories(places)
 
-for filename in originals:
-    place_name = extract_place(filename)
-    os.rename(filename, os.path.join(place_name, filename))
+organize_photos("Photos")
