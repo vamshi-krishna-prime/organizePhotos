3c430525a9003ea261cb6abfa1eb81cf1f1eaa3d
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-03 23:41:27 +0530

    Update organizePhotos.py

--- organizePhotos.py
+++ organizePhotos.py
@@ -10,3 +10,14 @@ os.chdir("c:\\Users\VAMSI\Desktop\Python\organize photos")
 filename = "sample.txt"
 path = "Photos"
 os.rename(filename, os.path.join(path, filename))
+
+def extract_place(filename):
+    first = filename.find("_")
+    partial = filename[first + 1 : ]
+    second = partial.find("_")
+    folder_name = partial[:second]
+    return folder_name
+  
+print(extract_place("2016-11-04_Berlin_09/42/22.jpg"))
+print(extract_place("2018-01-03_Oahu_21/51/57.jpg"))
+print(extract_place("2018-01_Scottland_11/51/27.jpg"))
