354c7ec4cfa20b52027c0e913e9e6dcef3789f5d
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-03 23:43:06 +0530

    Update organizePhotos.py

--- organizePhotos.py
+++ organizePhotos.py
@@ -1,28 +1,26 @@
 import os
 
-source = os.path.abspath(os.path.join(os.sep, "Users", "VAMSI", "Desktop", "Python"))
-print(source)
-os.chdir(source)
-files = os.listdir()
-print(files)
-
-os.chdir("c:\\Users\VAMSI\Desktop\Python\organize photos")
-filename = "sample.txt"
-path = "Photos"
-os.rename(filename, os.path.join(path, filename))
-
 def extract_place(filename):
-    return filename.split('_')[1]
+    return filename.split("_")[1]
 
-originals = ['2018-01-03_Scotland_21:51:57.jpg',
-             '2016-09-04_Berlin_08:25:50.jpg',
-             '2018-08-01_Oahu_21:51:37.jpg']
+def make_directories(list):
+    for location in list:
+        os.mkdir(location)
 
+# navigating to the photos directory and listing the file names
+os.chdir("c:\\Users\VAMSI\Desktop\Python\organize photos\Photos")
+originals = os.listdir()
 print(originals)
-
 places = []
+# loop to extract place names
 for filename in originals:
-    place = extract_place(filename)
-    places.append(place)
+    place_name = extract_place(filename)
+    if place_name not in places:
+        places.append(place_name)
 
 print(places)
+make_directories(places)
+
+for filename in originals:
+    place_name = extract_place(filename)
+    os.rename(filename, os.path.join(place_name, filename))
