8d7de6f82a5c00569c13defef0315c897809ae8d
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-03 23:40:33 +0530

    Update organizePhotos.py

--- organizePhotos.py
+++ organizePhotos.py
@@ -1,7 +1,13 @@
 import os
 
-path = os.path.join("c:\\", "Users", "VAMSI", "Desktop", "Python")
-print(path)
-os.chdir(path)
+# path = os.path.join("c:\\", "Users", "VAMSI", "Desktop", "Python")
+source = os.path.abspath(os.path.join(os.sep, "Users", "VAMSI", "Desktop", "Python"))
+print(source)
+os.chdir(source)
+# os.chdir("c:\\Users\VAMSI\Desktop\Python\organize photos")
 files = os.listdir()
 print(files)
+
+# filename = "sample.txt"
+# new_path = "Photos"
+# os.rename(filename, os.path.join(new_path, filename))
